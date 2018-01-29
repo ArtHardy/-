17b1533dd0cf83337ea33d67eb741e34cf8c2db3
varankin anatoliy <varankin@elewise.com>
2018-01-29 21:39:14 +0300

    MVC

---
+++ Calculator/WebCalc/Controllers/MyController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebCalc.Controllers
+{
+    public class MyController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index(long id)
+        {
+            ViewData.Model = id.ToString();
+
+            ViewData["age"] = 30;
+            ViewData.Add("isAdmin", false);
+
+            ViewBag.lastname = "Varankin";
+
+            ViewBag.Cities = new string[] { "Москва", "Питер" };
+
+            return View("Name");
+        }
+
+        [HttpPost]
+        public ActionResult Index(string firstname)
+        {
+            ViewData["age"] = 30;
+            ViewData.Add("isAdmin", false);
+
+            ViewBag.lastname = "Varankin";
+
+            ViewBag.Cities = new string[] { "Москва", "Питер" };
+
+            return View("Name", model: firstname);
+        }
+    }
+}
---
+++ Calculator/WebCalc/Extensions/MyHelpers.cs
@@ -0,0 +1,21 @@
+﻿using System.Web.Mvc;
+
+namespace WebCalc.Extensions
+{
+    public static class MyHelpers
+    {
+        public static MvcHtmlString ListerFlat(this HtmlHelper html, string[] items)
+        {
+            var ul = new TagBuilder("ul");
+            foreach (var item in items)
+            {
+                var li = new TagBuilder("li");
+                li.InnerHtml = item;
+
+                ul.InnerHtml += li;
+            }
+            return MvcHtmlString.Create(ul.ToString());
+        }
+
+    }
+}
